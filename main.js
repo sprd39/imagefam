88497b49b77528de5b94f6fa5b760cdc7dc13160
sprd39 <76632782+sprd39@users.noreply.github.com>
2024-08-01 19:34:58 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -23,3 +23,19 @@ function modelLoaded()
 {
     console.log("modelLoaded");
 }
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+
+}
